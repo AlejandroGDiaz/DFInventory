aa9d4bdd29bb7328732c86a633611790f832c0ba
alejandro <alejandro.gdiazm@hotmail.com>
2020-10-14 16:06:01 -0700

    Update succes and failure

--- client/src/components/confirmation/Failure.js
+++ client/src/components/confirmation/Failure.js
@@ -4,18 +4,20 @@ import { Link } from "react-router-dom";
 const Failure = () => {
     return(
         <div className="ui container" style={{marginTop:"20px", textAlign:"center"}}>
-        <h1>Algo salió mal</h1>
-        <div>
-            <p style={{fontSize:"20px"}}>
-                El error puede estar asociado a:
-                <ol className="ui list">
-                    <li>El modelo no se encontró al momento de dar de alta el material</li>
-                    <li>El modelo no se encontró al momento de dar de baja el material</li>
-                    <li>El número de cotización no existe o no ha sido dada de alta la oden de compra</li>
-                    <li>Algún modelo no está dado de alta en la orden de compra ingresada</li>
-                </ol>
-            </p>
-        </div>
+            <div className="ui negative message">
+                <h1>¡Algo salió mal!</h1>
+            </div>
+            <div>
+                <p style={{fontSize:"20px"}}>
+                    El error puede estar asociado a:
+                    <ol className="ui list">
+                        <li>El modelo no se encontró al momento de dar de alta el material</li>
+                        <li>El modelo no se encontró al momento de dar de baja el material</li>
+                        <li>El número de cotización no existe o no ha sido dada de alta la oden de compra</li>
+                        <li>Algún modelo no está dado de alta en la orden de compra ingresada</li>
+                    </ol>
+                </p>
+            </div>
         <Link className="ui button centered" style={{marginTop:"20px"}} to="/">Menú</Link>
     </div>
     )
--- client/src/components/confirmation/Success.js
+++ client/src/components/confirmation/Success.js
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 const Success = () => {
     return(
     <div className="ui container" style={{marginTop:"20px", textAlign:"center"}}>
-        <h1>Operación Exitosa</h1>
+        <div className="ui positive message">
+            <h1>¡Operación Exitosa!</h1>
+        </div>
         <div>
             <p style={{fontSize:"20px"}}>
                 La operación fue realizada con éxito. Presiona el botón para realizar una nueva
